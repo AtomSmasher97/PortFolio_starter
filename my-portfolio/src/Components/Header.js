5d276368a08d819f49a5acb5eed26de98aa2e913
Venkateswar Reddy Kothapally <venkateshwarreddykothapally@gmail.com>
2025-04-04 00:07:32 -0400

    commit1

--- my-portfolio/src/Components/Header.js
+++ my-portfolio/src/Components/Header.js
@@ -1,33 +1,25 @@
-import React, { useState } from "react";
-import './Header.css';
+import React from "react";
+import './Header.css'; 
+import './wavyLink.css'; 
+
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+    return( 
+        <header className="header">
+            <div className="logo">
+                <h1> Venkateswar Reddy Kothapally </h1>
+            </div>
+            <nav> 
+            <ul>
+                    <li><a href="#home" class="wave-link">HOME</a></li>
+                    <li><a href="#about" class="wave-link">About</a></li>
+                    <li><a href="#projects" class="wave-link">Projects</a></li>
+                    <li><a href="#contact" class="wave-link">Contact</a></li>
+                </ul>
+            </nav>
+               </header>
+        
+        );
+}
 
-  const toggleMenu = () => {
-    setIsMenuOpen(prev => !prev);
-  };
-
-  return (
-    <header className="header">
-      <div className="logo">
-        <h1>MY PORTFOLIO WEBSITE</h1>
-      </div>
-      <nav>
-        <div className="menu-toggle" onClick={toggleMenu}>
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
-        </div>
-        <ul className={`nav-links ${isMenuOpen ? "active" : ""}`}>
-          <li><a href="#home">HOME</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
-
-export default Header;
+export default Header; 
