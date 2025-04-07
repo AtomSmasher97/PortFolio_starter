e23e9489bc542a660f90d3cac7f97d0faf88970d
Venkateswar Reddy Kothapally <venkateshwarreddykothapally@gmail.com>
2025-04-07 17:33:18 -0400

    adding background video support

--- my-portfolio/src/Components/BackgroundVideo.js
+++ my-portfolio/src/Components/BackgroundVideo.js
@@ -5,9 +5,9 @@ const BackgroundVideo = () => {
   return (
     <>
     <video autoPlay loop muted playsInline id="background-video">
-      <source src="/playback.webm" type="video/webm" />
-      Your browser does not support the video tag.
-    </video>
+  <source src={`${process.env.PUBLIC_URL}/playback.webm`} type="video/webm" />
+  Your browser does not support the video tag.
+</video>
     <div className="blur-overlay"></div>
     <div className="grain-overlay"></div>
     </> 
